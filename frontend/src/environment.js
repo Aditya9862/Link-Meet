b51b4360bb9fe621d74fefc760c07beef4e00aa5
Aditya9862 <167546017+Aditya9862@users.noreply.github.com>
2026-06-24 00:33:27 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ? 
-    "https://link-meet-lifh.onrender.com :
+    "https://link-meet-lifh.onrender.com" :
     "http://localhost:8000"
 
 export default server;
