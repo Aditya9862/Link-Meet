dc49d5a590c48bcc9fba50a8d8627b010842f2c1
Aditya9862 <167546017+Aditya9862@users.noreply.github.com>
2026-06-24 00:19:00 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,6 @@
-let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
-    "http://localhost:8000"
-
+const server = {
+    dev: "http://localhost:8000"
+    prod: "https://link-meet-lifh.onrender.com"
+}
 
 export default server;
