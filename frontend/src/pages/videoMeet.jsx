6d4db66721ef30bff8b82f42d6853faa8ec0acaf
Aditya9862 <167546017+Aditya9862@users.noreply.github.com>
2026-06-24 01:38:30 +0530

    Update videoMeet.jsx

--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -523,27 +523,25 @@ export default function VideoMeetComponent() {
                     </div>
 
 
-                    <video className={styles.meetUserVideo} ref={localVideoref} autoPlay muted></video>
-
                     <div className={styles.conferenceView}>
-                        {videos.map((video) => (
-                            <div key={video.socketId}>
-                                <video
-
-                                    data-socket={video.socketId}
-                                    ref={ref => {
-                                        if (ref && video.stream) {
-                                            ref.srcObject = video.stream;
-                                        }
-                                    }}
-                                    autoPlay
-                                >
-                                </video>
-                            </div>
-
-                        ))}
-
-                    </div>
+    {videos.map((video) => (
+        <div
+            className={styles.videoTile}
+            key={video.socketId}
+        >
+            <video
+                data-socket={video.socketId}
+                ref={(ref) => {
+                    if (ref && video.stream) {
+                        ref.srcObject = video.stream;
+                    }
+                }}
+                autoPlay
+                playsInline
+            />
+        </div>
+    ))}
+</div>
 
                 </div>
 
