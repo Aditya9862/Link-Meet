cedbb1fcb6bbcdfb5afdf69964825126e4653c86
Aditya9862 <167546017+Aditya9862@users.noreply.github.com>
2026-06-24 01:50:45 +0530

    Refactor video rendering in videoMeet component

--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -523,21 +523,29 @@ export default function VideoMeetComponent() {
                     </div>
 
 
+                    <video className={styles.meetUserVideo} ref={localVideoref} autoPlay muted></video>
+
                     <div className={styles.conferenceView}>
-    {videos.map((video) => (
-        <div key={video.socketId}>
-            <video
-                data-socket={video.socketId}
-                ref={ref => {
-                    if (ref && video.stream) {
-                        ref.srcObject = video.stream;
-                    }
-                }}
-                autoPlay
-            />
-        </div>
-    ))}
-</div>
+                        {videos.map((video) => (
+                            <div key={video.socketId}>
+                                <video
+
+                                    data-socket={video.socketId}
+                                    ref={ref => {
+                                        if (ref && video.stream) {
+                                            ref.srcObject = video.stream;
+                                        }
+                                    }}
+                                    autoPlay
+                                >
+                                </video>
+                            </div>
+
+                        ))}
+
+                    </div>
+
+                </div>
 
             }
 
